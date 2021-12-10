17b3805d2f2e3366595a43ddd765960a959acd97
LeeJeongGi <apple4rhk@naver.com>
2021-12-10 13:45:25 +0900

    feat : 선택 정렬 알고리즘

--- AlgoritymStudy/src/Selection_Sort.java
+++ AlgoritymStudy/src/Selection_Sort.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Selection_Sort {
+import java.util.Arrays;
+
+public class Selection_Sort {
+
+    private static void selectionSort(int[] sort, int size) {
+
+        for(int i = 0; i < size - 1; i++) {
+            int min_index = i;
+
+            for(int j = i + 1; j < size; j++) {
+                if(sort[j] < sort[min_index]) {
+                    min_index = j;
+                }
+            }
+
+            swap(sort, min_index, i);
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+        선택 정렬 : notion에 내용은 정리 했으니 참고 바람
+         */
+        int[] sort = {7,8,2,9,1};
+
+        selectionSort(sort, sort.length);
+
+        Arrays.stream(sort)
+                .forEach(System.out::println);
+    }
 }
