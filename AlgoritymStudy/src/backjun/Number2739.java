4b12f34530150478b2b3621b57eab556ef1387b4
LeeJeongGi <apple4rhk@naver.com>
2021-12-20 13:34:24 +0900

    feat : 백준 2739번 - 구구단

---
+++ AlgoritymStudy/src/backjun/Number2739.java
@@ -0,0 +1,14 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class Number2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int multiplication = sc.nextInt();
+
+        for(int i = 1; i < 10; i++) {
+            System.out.println(String.format("%d * %d = %d ", multiplication, i, multiplication * i));
+        }
+    }
+}
