100210dc0079db5739bda76f81a01fd490d466f2
LeeJeongGi <apple4rhk@naver.com>
2021-12-21 15:42:21 +0900

    feat : 2742번 - N 거꾸로 찍기

---
+++ AlgoritymStudy/src/backjun/Number2742.java
@@ -0,0 +1,18 @@
+package backjun;
+
+import java.io.*;
+
+public class Number2742 {
+    public static void two742_main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = N; i >= 1; i--) {
+            sb.append(i + "\n");
+        }
+        System.out.println(sb);
+    }
+}
