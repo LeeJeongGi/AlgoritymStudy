32c93c0157d2de857eedd0c0ccc25e9df9ff50c8
LeeJeongGi <apple4rhk@naver.com>
2021-12-21 16:03:21 +0900

    feat : A와 B를 입력 받아 결과 출력2

---
+++ AlgoritymStudy/src/backjun/Number11022.java
@@ -0,0 +1,25 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number11022 {
+    public static void one1022_main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= T; i++) {
+            String[] input = br.readLine().split(" ");
+
+            int first = Integer.parseInt(input[0]);
+            int second = Integer.parseInt(input[1]);
+            int sum = first + second;
+
+            sb.append(String.format("Case #%d: %d + %d = %d\n", i, first, second, sum));
+        }
+
+        System.out.print(sb);
+    }
+}
