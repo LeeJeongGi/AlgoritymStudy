f82361c0b8203fc9fb0fa2b0e24cc7e29a65d90b
LeeJeongGi <apple4rhk@naver.com>
2021-12-22 15:35:26 +0900

    feat : A + B while 문으로 출력

---
+++ AlgoritymStudy/src/backjun/easy/Number10952.java
@@ -0,0 +1,21 @@
+package backjun.easy;
+
+import java.util.Scanner;
+
+public class Number10952 {
+    public static void one0952_main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if(A == 0 && B == 0) {
+                sc.close();
+                break;
+            }
+
+            System.out.println(A + B);
+        }
+    }
+}
