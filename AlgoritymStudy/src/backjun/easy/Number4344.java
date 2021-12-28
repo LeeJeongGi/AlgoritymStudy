176f1836f8d38656384fd296a59236d925e7b4a0
LeeJeongGi <apple4rhk@naver.com>
2021-12-28 15:56:00 +0900

    feat : 평균은 넘겠지 - 평균 넘는 학생의 실제 비율

---
+++ AlgoritymStudy/src/backjun/easy/Number4344.java
@@ -0,0 +1,34 @@
+package backjun.easy;
+
+import java.util.Scanner;
+
+public class Number4344 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int C = sc.nextInt();
+        int[] scores;
+        for(int i = 0; i < C; i++) {
+
+            int person = sc.nextInt();
+            scores = new int[person];
+
+            double sum = 0;
+            for(int j = 0; j < person; j ++) {
+                int value = sc.nextInt();
+                scores[j] = value;
+                sum += value;
+            }
+
+            double avg = sum / person;
+            double avgOverPerson = 0;
+
+            for(int j = 0; j < person; j++) {
+                if(scores[j] > avg) {
+                    avgOverPerson++;
+                }
+            }
+            System.out.printf("%.3f%%\n", (avgOverPerson/person)*100);
+        }
+    }
+}
