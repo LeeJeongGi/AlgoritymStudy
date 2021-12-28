928e95c751bffe9b002f4732a6b3b515e054e54f
LeeJeongGi <apple4rhk@naver.com>
2021-12-28 14:21:34 +0900

    feat : 42로 나눈 나머지 중 겹치지 않는 숫자의 갯수

---
+++ AlgoritymStudy/src/backjun/easy/Number3052.java
@@ -0,0 +1,21 @@
+package backjun.easy;
+
+import java.util.*;
+
+public class Number3052 {
+    public static void Three052_main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] inputData = new int[10];
+        for(int i = 0; i < inputData.length; i++) {
+            inputData[i] = sc.nextInt();
+        }
+
+        Set<Integer> resultValues = new TreeSet<>();
+        for(int i = 0; i < inputData.length; i++) {
+            resultValues.add(inputData[i] % 42);
+        }
+
+        System.out.println(resultValues.size());
+    }
+}
