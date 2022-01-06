75019c33cea3a99baa44da3bd602bbc33397e0f7
LeeJeongGi <apple4rhk@naver.com>
2022-01-06 22:47:01 +0900

    feat : 문자 반복 - 특정 숫자와 문자를 입력받아 해당 숫자만큼 문자열 복제하여 출력하는 문제

---
+++ AlgoritymStudy/src/backjun/easy/Number2675.java
@@ -0,0 +1,24 @@
+package backjun.easy;
+
+import java.util.Scanner;
+
+public class Number2675 {
+    public static void two675_main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+
+        for(int i = 0; i < testCase; i++) {
+            int number = sc.nextInt();
+            String letter = sc.next();
+
+            for(int j = 0; j < letter.length(); j++) {
+                for(int k = 0; k < number; k++) {
+                    System.out.print(letter.charAt(j));
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
