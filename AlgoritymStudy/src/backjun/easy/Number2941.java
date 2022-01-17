de0abde85ddb5e61663f424f46867a608cafce11
LeeJeongGi <apple4rhk@naver.com>
2022-01-17 16:52:38 +0900

    feat : 크로아티아 알파벳 - 특정 알파벳을 포함하고 있으면 한 문자로 취급후 길이 계산

---
+++ AlgoritymStudy/src/backjun/easy/Number2941.java
@@ -0,0 +1,19 @@
+package backjun.easy;
+
+import java.util.Scanner;
+
+public class Number2941 {
+    public static void two941_main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] croatiaMsg = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        String inputData = sc.nextLine();
+
+        for(int i = 0; i < croatiaMsg.length; i++) {
+            if(inputData.contains(croatiaMsg[i])) {
+                inputData = inputData.replace(croatiaMsg[i], "#");
+            }
+        }
+        System.out.println(inputData.length());
+    }
+}
