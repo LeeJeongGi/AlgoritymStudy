8480aa8c38748072e5218d69d7785b69fb543aca
LeeJeongGi <apple4rhk@naver.com>
2022-04-06 21:49:28 +0900

    feat : class move

---
+++ src/effectiveJava/chapter2/item2/item2.java
@@ -0,0 +1,14 @@
+package src.effectiveJava.chapter2.item2;
+
+public class item2 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public static Boolean valueOf(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+    }
+}
--- src/effectiveJava/item2.java
+++
@@ -1,7 +0,0 @@
-package src.effectiveJava;
-
-public class item2 {
-    public static void main(String[] args) {
-
-    }
-}
