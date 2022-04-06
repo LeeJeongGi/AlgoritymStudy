7b1bf6fc91c6b80fb43f14cc1af58a9df38d9eb0
LeeJeongGi <apple4rhk@naver.com>
2022-04-06 21:49:50 +0900

    feat : 매개변수가 많을 때 사용하는 점진적 방법

---
+++ src/effectiveJava/chapter2/item2/NutritionFacts.java
@@ -0,0 +1,44 @@
+package src.effectiveJava.chapter2.item2;
+
+public class NutritionFacts {
+
+    private final int servingSize;  // (mL, 1회 제공량)     필수
+    private final int servings;     // (회, 총 n회 제공량)  필수
+    private final int calories;     // (1회 제공량당)       선택
+    private final int fat;          // (g/1회 제공량)       선택
+    private final int sodium;       // (mg/1회 제공량)      선택
+    private final int carbohydrate; // (g/1회 제공량)       선택
+
+    public NutritionFacts(int servingSize, int servings) {
+        this(servingSize, servings, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories) {
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat, int sodium) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat, int sodium, int carbohydrate) {
+        this.servingSize  = servingSize;
+        this.servings     = servings;
+        this.calories     = calories;
+        this.fat          = fat;
+        this.sodium       = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts cocaCola =
+                new NutritionFacts(240, 8, 100, 0, 35, 27);
+    }
+}
