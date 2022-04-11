495bd8589a0618e95f969837d3693985b234cd33
LeeJeongGi <apple4rhk@naver.com>
2022-04-11 22:13:51 +0900

    item14 - Comparable Test Code 작성

---
+++ src/effectiveJava/chapter3/item14/WordList.java
@@ -0,0 +1,16 @@
+package src.effectiveJava.chapter3.item14;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+// Comparable 구현 시의 이점 (87쪽)
+public class WordList {
+    public static void main(String[] args) {
+
+        Set<String> s = new TreeSet<>();
+        Collections.addAll(s, args);
+
+        System.out.println(s);
+    }
+}
