99d05a729d3fde39c684d61a7b2389e582dc1987
LeeJeongGi <apple4rhk@naver.com>
2022-04-21 21:51:46 +0900

    Item25 - 톱레벨 클래스는 한 파일에 하나만 담자

---
+++ src/effectiveJava/chapter4/item25/ItemMain.java
@@ -0,0 +1,7 @@
+package src.effectiveJava.chapter4.item25;
+
+public class ItemMain {
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
