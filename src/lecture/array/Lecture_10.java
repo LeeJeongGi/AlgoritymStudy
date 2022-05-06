892811eae8b9acf9fcf7b6fbc4175bfc476a2045
LeeJeongGi <apple4rhk@naver.com>
2022-05-06 13:42:53 +0900

    feat : 봉우리 문제 결과

--- src/lecture/array/Lecture_10.java
+++ src/lecture/array/Lecture_10.java
@@ -3,6 +3,9 @@ package src.lecture.array;
 import java.util.Scanner;
 
 public class Lecture_10 {
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) {
 
         /**
@@ -12,5 +15,43 @@ public class Lecture_10 {
          * 만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
          */
         Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(solution(N, board));
+    }
+
+    private static int solution(int N, int[][] board) {
+
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+
+                boolean flag = true;
+
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if(nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] >= board[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
     }
 }
