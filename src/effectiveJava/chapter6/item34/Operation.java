70eaedc6bf7c3896b14ca4035d5529b43994525a
LeeJeongGi <apple4rhk@naver.com>
2022-05-27 22:43:49 +0900

    feat : Item34 : enum Type

---
+++ src/effectiveJava/chapter6/item34/Operation.java
@@ -0,0 +1,50 @@
+package src.effectiveJava.chapter6.item34;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+public enum Operation {
+
+    PLUS("+") {
+        public double apply(double x, double y) { return x + y; }
+    },
+    MINUS("-") {
+        public double apply(double x, double y) { return x - y; }
+    },
+    TIMES("*") {
+        public double apply(double x, double y) { return x * y; }
+    },
+    DIVIDE("/") {
+        public double apply(double x, double y) { return x / y; }
+    };
+
+    private final String symbol;
+
+    Operation(String symbol) { this.symbol = symbol; }
+
+    @Override public String toString() { return symbol; }
+
+    public abstract double apply(double x, double y);
+
+    // 코드 34-7 열거 타입용 fromString 메서드 구현하기 (216쪽)
+    private static final Map<String, Operation> stringToEnum =
+            Stream.of(values()).collect(
+                    toMap(Object::toString, e -> e));
+
+    // 지정한 문자열에 해당하는 Operation을 (존재한다면) 반환한다.
+    public static Optional<Operation> fromString(String symbol) {
+        return Optional.ofNullable(stringToEnum.get(symbol));
+    }
+
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        for (Operation op : Operation.values())
+            System.out.printf("%f %s %f = %f%n",
+                    x, op, y, op.apply(x, y));
+    }
+
+}
