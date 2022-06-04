4e50c2bca268d4c17aabc65dedcbeab8db115ffc
LeeJeongGi <apple4rhk@naver.com>
2022-06-04 22:00:30 +0900

    feat : EnumSet - 비트 필드를 대체하는 현대적 기법

---
+++ src/effectiveJava/chapter6/item36/Text.java
@@ -0,0 +1,22 @@
+package src.effectiveJava.chapter6.item36;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+// 코드 36-2 EnumSet - 비트 필드를 대체하는 현대적 기법 (224쪽)
+public class Text {
+    public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
+
+    // 어떤 Set을 넘겨도 되나, EnumSet이 가장 좋다.
+    public void applyStyles(Set<Style> styles) {
+        System.out.printf("Applying styles %s to text%n",
+                Objects.requireNonNull(styles));
+    }
+
+    // 사용 예
+    public static void main(String[] args) {
+        Text text = new Text();
+        text.applyStyles(EnumSet.of(Style.BOLD, Style.ITALIC));
+    }
+}
