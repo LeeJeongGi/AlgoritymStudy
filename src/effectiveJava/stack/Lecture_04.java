9cb04167c9d64884f739eac6470657d8c05e359a
LeeJeongGi <apple4rhk@naver.com>
2022-06-20 23:35:02 +0900

    feat : 후위식 연산

--- src/effectiveJava/stack/Lecture_04.java
+++ src/effectiveJava/stack/Lecture_04.java
@@ -1,5 +1,7 @@
 package src.effectiveJava.stack;
 
+import java.util.Scanner;
+
 public class Lecture_04 {
     public static void main(String[] args) {
 
@@ -7,6 +9,7 @@ public class Lecture_04 {
          * 후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
          * 만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 12입니다.
          */
+        Scanner sc = new Scanner(System.in);
 
     }
 }
