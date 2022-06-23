6d1df0457be36c5b887f28128ce9740581133f28
LeeJeongGi <apple4rhk@naver.com>
2022-06-23 22:50:11 +0900

    feat : 후위식 연산 풀이

--- src/effectiveJava/stack/Lecture_04.java
+++ src/effectiveJava/stack/Lecture_04.java
@@ -1,6 +1,7 @@
 package src.effectiveJava.stack;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Lecture_04 {
     public static void main(String[] args) {
@@ -10,6 +11,38 @@ public class Lecture_04 {
          * 만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 12입니다.
          */
         Scanner sc = new Scanner(System.in);
+        String input = sc.next();
 
+        System.out.println(solution(input));
+
+    }
+
+    private static int solution(String input) {
+
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (char x : input.toCharArray()) {
+
+            if (Character.isDigit(x)) {
+                stack.push(x - 48);
+            } else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+
+                if (x == '+') {
+                    stack.push(lt + rt);
+                } else if (x == '-') {
+                    stack.push(lt - rt);
+                } else if (x == '*') {
+                    stack.push(lt * rt);
+                } else if (x == '/') {
+                    stack.push(lt / rt);
+                }
+            }
+        }
+        answer = stack.pop();
+
+        return answer;
     }
 }
