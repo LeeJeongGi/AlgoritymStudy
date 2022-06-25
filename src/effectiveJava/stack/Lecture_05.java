7def91f432742df1aa72c5a52a3dbb9ec059e65b
LeeJeongGi <apple4rhk@naver.com>
2022-06-25 21:38:28 +0900

    feat : 쇠 막대기 문제

---
+++ src/effectiveJava/stack/Lecture_05.java
@@ -0,0 +1,14 @@
+package src.effectiveJava.stack;
+
+import java.util.Scanner;
+
+public class Lecture_05 {
+    public static void main(String[] args) {
+
+        /**
+         * 여러 개의 쇠막대기를 레이저로 절단하려고 한다. 효율적인 작업을 위해서 쇠막대기를 아래에서 위로 겹쳐 놓고,
+         * 레이저를 위에서 수직으로 발사하여 쇠막대기들을 자른다.
+         */
+        Scanner sc = new Scanner(System.in);
+    }
+}
