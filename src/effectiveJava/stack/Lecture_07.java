d40b8d78e31638b33a15de8ab79339bfa15ebfb4
LeeJeongGi <apple4rhk@naver.com>
2022-06-27 16:50:57 +0900

    feat : 교육과정 설계 문제

---
+++ src/effectiveJava/stack/Lecture_07.java
@@ -0,0 +1,45 @@
+package src.effectiveJava.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Lecture_07 {
+    public static void main(String[] args) {
+
+        /**
+         * 교육과정 설계
+         */
+        Scanner sc = new Scanner(System.in);
+        String condition = sc.next();
+        String input = sc.next();
+
+
+        System.out.println(solution(condition, input));
+    }
+
+    private static String solution(String condition, String input) {
+
+        String answer = "YES";
+
+        Queue<Character> conditionQ = new LinkedList<>();
+
+        for (char ch : condition.toCharArray()) {
+            conditionQ.offer(ch);
+        }
+
+        for (char ch : input.toCharArray()) {
+
+            if (conditionQ.contains(ch)) {
+                if (conditionQ.poll() != ch) {
+                    return "NO";
+                }
+            }
+        }
+        if (!conditionQ.isEmpty()) {
+            return "NO";
+        }
+
+        return answer;
+    }
+}
