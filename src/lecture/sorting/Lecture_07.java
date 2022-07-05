d21e6398bc5b2e50bda57eea16475e92e2d183cb
LeeJeongGi <apple4rhk@naver.com>
2022-07-05 23:47:54 +0900

    feat : 좌표 정렬

---
+++ src/lecture/sorting/Lecture_07.java
@@ -0,0 +1,12 @@
+package src.lecture.sorting;
+
+public class Lecture_07 {
+    public static void main(String[] args) {
+
+        /**
+         * N개의 평면상의 좌표(x, y)가 주어지면 모든 좌표를 오름차순으로 정렬하는 프로그램을 작성하세요.
+         * 정렬기준은 먼저 x값의 의해서 정렬하고, x값이 같을 경우 y값에 의해 정렬합니다.
+         */
+
+    }
+}
