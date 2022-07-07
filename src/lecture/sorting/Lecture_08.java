72bac0a47111089d5f1a611e16a0b533b75ca4b7
LeeJeongGi <apple4rhk@naver.com>
2022-07-07 22:51:51 +0900

    feat : 이분 검색

---
+++ src/lecture/sorting/Lecture_08.java
@@ -0,0 +1,12 @@
+package src.lecture.sorting;
+
+public class Lecture_08 {
+    public static void main(String[] args) {
+
+        /**
+         * 임의의 N개의 숫자가 입력으로 주어집니다. N개의 수를 오름차순으로 정렬한 다음 N개의 수 중 한 개의 수인 M이 주어지면
+         * 이분검색으로 M이 정렬된 상태에서 몇 번째에 있는지 구하는 프로그램을 작성하세요. 단 중복값은 존재하지 않습니다.
+         */
+
+    }
+}
