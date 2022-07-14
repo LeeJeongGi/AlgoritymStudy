add95d69fb4a70a0096ceaf0233123c2d6643cb0
LeeJeongGi <apple4rhk@naver.com>
2022-07-14 23:31:03 +0900

    feat : 재귀 함수

---
+++ src/lecture/tree/Lecture_01.java
@@ -0,0 +1,14 @@
+package src.lecture.tree;
+
+import java.util.Scanner;
+
+public class Lecture_01 {
+    public static void main(String[] args) {
+
+        /**
+         * 재귀함수
+         */
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
