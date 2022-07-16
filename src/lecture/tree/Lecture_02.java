07059f9d08917fe4fb58b4de6023d5df71a90689
LeeJeongGi <apple4rhk@naver.com>
2022-07-16 21:55:32 +0900

    feat : DFS 합이 같은 부분 집합

---
+++ src/lecture/tree/Lecture_02.java
@@ -0,0 +1,17 @@
+package src.lecture.tree;
+
+import java.util.Scanner;
+
+public class Lecture_02 {
+    public static void main(String[] args) {
+
+        /**
+         * DFS
+         */
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+    }
+}
