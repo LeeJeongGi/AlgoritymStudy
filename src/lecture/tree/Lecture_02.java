f5deadea875d67ed31ab791965806cafb93a7d55
LeeJeongGi <apple4rhk@naver.com>
2022-07-18 21:16:57 +0900

    feat : DFS 합이 같은 부분 집합 풀이

--- src/lecture/tree/Lecture_02.java
+++ src/lecture/tree/Lecture_02.java
@@ -3,6 +3,11 @@ package src.lecture.tree;
 import java.util.Scanner;
 
 public class Lecture_02 {
+
+    static String answer = "NO";
+    static int N, total = 0;
+    static boolean flag = false;
+
     public static void main(String[] args) {
 
         /**
@@ -10,8 +15,33 @@ public class Lecture_02 {
          */
         Scanner sc = new Scanner(System.in);
 
+        N = sc.nextInt();
+        int[] values = new int[N];
+        for (int i = 0; i < N; i++) {
+            values[i] = sc.nextInt();
+            total += values[i];
+        }
 
+        DFS(0, 0, values);
 
+        System.out.println(answer);
+    }
+    public static void DFS(int L, int sum, int[] arr) {
 
+        if (flag) {
+            return;
+        }
+
+        if (L == N) {
+
+            if ((total - sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
     }
 }
