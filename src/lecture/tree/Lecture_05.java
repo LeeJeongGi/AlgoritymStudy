2d06488e345c0eb84300c16624c7b03b2550ecf1
LeeJeongGi <apple4rhk@naver.com>
2022-08-01 22:06:52 +0900

    feat : DFS - 중복순열 문제 풀이

--- src/lecture/tree/Lecture_05.java
+++ src/lecture/tree/Lecture_05.java
@@ -3,11 +3,31 @@ package src.lecture.tree;
 import java.util.Scanner;
 
 public class Lecture_05 {
+
+    static int[] pm;
+    static int n,m;
     public static void main(String[] args) {
         /**
          * 중복순열 구하기
          */
         Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        pm = new int[m];
+        DFS(0);
+    }
 
+    public static void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 1; i <= n; i++) {
+                pm[L] = i;
+                DFS(L + 1);
+            }
+        }
     }
 }
