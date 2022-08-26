9770f66dcddeb675220b6234dee1f6e3c3d625fb
LeeJeongGi <apple4rhk@naver.com>
2022-08-26 23:34:06 +0900

    feat : 그리디 알고리즘 친구인가?

---
+++ src/lecture/greed/Lecture_05.java
@@ -0,0 +1,12 @@
+package src.lecture.greed;
+
+import java.util.Scanner;
+
+public class Lecture_05 {
+    public static void main(String[] args) {
+        /**
+         * Greed : 친구인가?
+         */
+        Scanner sc = new Scanner(System.in);
+    }
+}
