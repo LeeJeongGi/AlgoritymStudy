47dc54a07b4d4348923354f844590a97feb27f23
LeeJeongGi <apple4rhk@naver.com>
2022-08-28 23:13:25 +0900

    feat : 그리디 알고리즘 원더랜드

---
+++ src/lecture/greed/Lecture_06.java
@@ -0,0 +1,12 @@
+package src.lecture.greed;
+
+import java.util.Scanner;
+
+public class Lecture_06 {
+    public static void main(String[] args) {
+        /**
+         * 원더랜드
+         */
+        Scanner sc = new Scanner(System.in);
+    }
+}
