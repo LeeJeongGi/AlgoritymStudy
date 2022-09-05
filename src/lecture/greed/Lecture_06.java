cfefbae857deed621706dbdb493f3e6390b44c3f
LeeJeongGi <apple4rhk@naver.com>
2022-09-05 23:16:22 +0900

    feat : 원더랜드 - 크루스카 알고리즘

--- src/lecture/greed/Lecture_06.java
+++ src/lecture/greed/Lecture_06.java
@@ -1,12 +1,82 @@
 package src.lecture.greed;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Lecture_06 {
+
+    static int[] unf;
+
     public static void main(String[] args) {
         /**
          * 원더랜드
          */
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        unf = new int[n + 1];
+
+        ArrayList<Edge> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            unf[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            arr.add(new Edge(a, b, c));
+        }
+
+        int answer = 0;
+        Collections.sort(arr);
+        for (Edge ob : arr) {
+            int fv1 = Find(ob.v1);
+            int fv2 = Find(ob.v2);
+
+            if (fv1 != fv2) {
+                answer += ob.cost;
+                Union(ob.v1, ob.v2);
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static int Find(int v) {
+        if (v == unf[v]) {
+            return v;
+        } else {
+            return unf[v] = Find(unf[v]);
+        }
+    }
+
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+
+        if (fa != fb) {
+            unf[fa] = fb;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int v1;
+        int v2;
+        int cost;
+
+        public Edge(int v1, int v2, int cost) {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
     }
 }
