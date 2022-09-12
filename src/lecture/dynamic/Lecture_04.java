77e649848742b3797f92d24b694bc56024e2e40e
LeeJeongGi <apple4rhk@naver.com>
2022-09-12 22:01:42 +0900

    feat : Dynamic 가장 높은 벽돌 탑 쌓기

---
+++ src/lecture/dynamic/Lecture_04.java
@@ -0,0 +1,65 @@
+package src.lecture.dynamic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Lecture_04 {
+
+    static int[] dy;
+
+    public static void main(String[] args) {
+        /**
+         * 가장 높은 탑 쌓기
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Brick> arr = new ArrayList<>();
+        dy = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            arr.add(new Brick(a, b, c));
+        }
+        System.out.println(solution(arr));
+    }
+
+    public static int solution(ArrayList<Brick> arr) {
+        int answer = 0;
+
+        Collections.sort(arr);
+        dy[0] = arr.get(0).h;
+
+        answer = dy[0];
+        for (int i = 1; i < arr.size(); i++) {
+            int max_h = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr.get(j).w > arr.get(i).w && dy[j] > max_h) {
+                    max_h = dy[j];
+                }
+            }
+            dy[i] = max_h + arr.get(i).h;
+            answer = Math.max(answer, dy[i]);
+        }
+
+        return answer;
+    }
+
+    static class Brick implements Comparable<Brick>{
+        int s,h, w;
+
+        public Brick(int s, int h, int w) {
+            this.s = s;
+            this.h = h;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Brick o) {
+            return o.s - this.s;
+        }
+    }
+}
