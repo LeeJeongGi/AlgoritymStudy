0a43729e9b190f706eb87fa45b52415297f5e2b3
LeeJeongGi <apple4rhk@naver.com>
2022-10-13 15:41:48 +0900

    feat : 최솟값 구하기

---
+++ src/backjun/easy/Number11003.java
@@ -0,0 +1,54 @@
+package src.backjun.easy;
+
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Number11003 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * 최솟값 찾기
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        Deque<Node> myDeque = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            // 마지막 값을 지워 준다.
+            while (!myDeque.isEmpty() && myDeque.getLast().value > now) {
+                myDeque.removeLast();
+            }
+
+            // deque 에 새로운 값은 넣어준다.
+            myDeque.addLast(new Node(i, now));
+
+            // index 가 초과했는지 여부 검사
+            // 2 -> 1~4 = 4 > 3
+            if (myDeque.getFirst().index <= i - L) {
+                myDeque.removeFirst();
+            }
+
+            bw.write(myDeque.getFirst().value + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static class Node {
+        int index;
+        int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+    }
+}
