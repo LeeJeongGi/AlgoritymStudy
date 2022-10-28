2f44c82155698b27322ca021ec0b37198f674950
LeeJeongGi <apple4rhk@naver.com>
2022-10-28 21:49:51 +0900

    feat : test 용 파일

---
+++ src/theory/InputTest.java
@@ -0,0 +1,18 @@
+package src.theory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+        /**
+         * 블로그 정리하면서 쓰는 코드 정리
+         */
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        int read = inputStreamReader.read();
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+    }
+}
