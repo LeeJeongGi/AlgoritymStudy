2839c5a45918e7731a45bc807021e7b604c9d1c0
LeeJeongGi <apple4rhk@naver.com>
2022-11-02 18:40:18 +0900

    next Max number Solution

---
+++ src/programmers/NextMaxNumber.java
@@ -0,0 +1,21 @@
+package src.programmers;
+
+public class NextMaxNumber {
+    public static void main(String[] args) {
+        /**
+         * 다음 큰 숫자
+         */
+        int n = 78;
+
+        int answer = 0;
+        int nCount = Integer.bitCount(n);
+        while (true) {
+            n++;
+            if (Integer.bitCount(n) == nCount) {
+                answer = n;
+                break;
+            }
+        }
+        System.out.println("answer = " + answer);
+    }
+}
