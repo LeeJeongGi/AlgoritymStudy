f4d30fc2bde1633e9012e80174c7b5a387c00830
LeeJeongGi <apple4rhk@naver.com>
2022-12-13 22:26:45 +0900

    feat : example

--- src/theory/Sample.java
+++ src/theory/Sample.java
@@ -1,6 +1,8 @@
 package src.theory;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Sample {
     public static void main(String[] args) {
@@ -9,6 +11,29 @@ public class Sample {
          * 코딩 테스트 복습하는 연습장
          */
         Scanner sc = new Scanner(System.in);
-        int[][] commands = new int[51][51];
+        int N = sc.nextInt();
+
+        int[] cache = new int[N + 1];
+        IntStream.range(0, N).forEach(i -> cache[i] = sc.nextInt());
+
+        int[] distance = new int[N + 1];
+
+        distance[0] = 1;
+        for (int i = 1; i < N; i++) {
+
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (cache[i] > cache[j] && distance[j] > max) {
+                    max = distance[j];
+                }
+            }
+
+            distance[i] = max + 1;
+        }
+
+
+        int answer = Arrays.stream(distance).max().getAsInt();
+
+        System.out.println(answer);
     }
 }
