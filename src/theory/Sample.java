23a04ed1367c57c44e0c653cc06b2792ec2efe13
LeeJeongGi <apple4rhk@naver.com>
2023-02-09 20:33:57 +0900

    feat : 코테 샘플

--- src/theory/Sample.java
+++ src/theory/Sample.java
@@ -1,39 +1,91 @@
 package src.theory;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Sample {
+
+    static int[][] patterns = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+
     public static void main(String[] args) {
 
         /**
          * 코딩 테스트 복습하는 연습장
          */
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int[] answer = {1, 3, 2, 4, 2};
+        System.out.println(solution(answer));
+    }
 
-        int[] cache = new int[N + 1];
-        IntStream.range(0, N).forEach(i -> cache[i] = sc.nextInt());
+    public static int[] solution(int[] answers) {
 
-        int[] distance = new int[N + 1];
-
-        distance[0] = 1;
-        for (int i = 1; i < N; i++) {
-
-            int max = 0;
-            for (int j = i - 1; j >= 0; j--) {
-                if (cache[i] > cache[j] && distance[j] > max) {
-                    max = distance[j];
-                }
-            }
-
-            distance[i] = max + 1;
+        ArrayList<Student> students = new ArrayList<>();
+        for (int i = 0; i < patterns.length; i++) {
+            students.add(new Student(i + 1, patterns[i]));
         }
 
+        // 정답 갯수 계산
+        for (int i = 0; i < students.size(); i++) {
+            Student tempStudent = students.get(i);
+            tempStudent.calculatorAnswerCount(answers, tempStudent.pattern);
+        }
 
-        int answer = Arrays.stream(distance).max().getAsInt();
+        // 최댓값 구하기
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < students.size(); i++) {
+            Student tempStudent = students.get(i);
+            maxValue = Math.max(maxValue, tempStudent.answerCount);
+        }
 
-        System.out.println(answer);
+        // 정답을 가장 많이 맞춘 학생 저장
+        ArrayList<Integer> tempAnswer = new ArrayList<>();
+        for (int i = 0; i < students.size(); i++) {
+            Student tempStudent = students.get(i);
+            if (maxValue == tempStudent.answerCount) {
+                tempAnswer.add(tempStudent.number);
+            }
+        }
+
+        int[] answer = tempAnswer.stream().mapToInt(integer -> integer).toArray();
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        return answer;
     }
+
+    private static class Student {
+        int number;
+        int[] pattern;
+
+        int answerCount;
+
+        public Student(int number, int[] pattern) {
+            this.number = number;
+            this.pattern = pattern;
+            this.answerCount = 0;
+        }
+
+        public void calculatorAnswerCount(int[] answers, int[] tempPattern) {
+            int tempCount = 0;
+
+            int answerOfLength = answers.length;
+            int patternOfLength = tempPattern.length;
+
+            int index = 0;
+            while (index < answerOfLength) {
+                if (answers[index] == tempPattern[index % patternOfLength]) {
+                    tempCount++;
+                }
+                index++;
+            }
+
+            this.answerCount = tempCount;
+        }
+    }
+
 }
